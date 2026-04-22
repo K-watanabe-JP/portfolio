44035ffb1dfc0abf9b78401028196dd35a770e39
Kazuyuki Watanabe <kz.watanabe.dev@gmail.com>
2026-04-22 13:44:22 +0900

    feat(navbar.jsx): enhance navigation menu with additional links and proper keying for list items

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,20 @@ const Navbar = () => {
     return (
         <nav>
             <div>
-                <img src="/images/logo.svg" />
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">K.watanabe's Portfolio</p>
+
+                <ul>
+                    {[
+                        { id: 1, name: "Portfolio" },
+                        { id: 2, name: "Contact" },
+                        { id: 3, name: "Projects" },
+                    ].map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </nav>
     );
