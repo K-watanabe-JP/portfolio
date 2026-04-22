9ba050d13daf6d61451ad2a8661e8e942c044301
Kazuyuki Watanabe <kz.watanabe.dev@gmail.com>
2026-04-22 18:00:26 +0900

    feat(components/Welcome.jsx): add support for accessibility by using refs for dynamic title and subtitle elements

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,17 @@
+import { useRef } from "react";
+
 const Welcome = () => {
-    return <div>Welcome</div>;
+    const titleRef = useRef(null);
+    const subtitleRef = useRef(null);
+
+    return <section id="welcome">
+        <p ref={subtitleRef}>Hi, I'm K.watanabe! Welcome to my</p>
+        <h1 ref={titleRef} className="mt-7">Portfolio</h1>
+        <div className="small-screen">
+            <p>This Portfolio is designed for desktop/tabled screens only.</p>
+            <p>Please use desktop/tabled to view this portfolio.</p>
+        </div>
+    </section>;
 };
 
 export default Welcome;
