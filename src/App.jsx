2190ae205fd5eb5f6814ddd655d46f82024b6994
Kazuyuki Watanabe <kz.watanabe.dev@gmail.com>
2026-05-13 18:41:40 +0900

    feat(App.jsx): add GSAP library import for animation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar, Welcome, Dock } from "#components";
+import gsap from "gsap";
+import { Draggable } from "gsap/all";
 
-import { Draggable } from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
 
 const App = () => {
