d2df11d11e1982bb89848af05e00530b74b36a39
Kazuyuki Watanabe <kz.watanabe.dev@gmail.com>
2026-05-17 16:58:36 +0900

    feat(hoc/WindowWrapper.jsx): add support for dynamic window rendering with ref and useWindowStore hook

---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,24 @@
+import { useRef } from "react";
+import useWindowStore from "#store/windows.js";
+
+const WindowWrapper = (Component, windowKey) => {
+    const Wrapped = (props) => {
+        const { focusWindow, windows } = useWindowStore();
+        const { isOpen, zIndex } = windows[windowKey];
+        const ref = useRef(null);
+
+        // if (!isOpen) return null;
+
+        return (
+            <section id={windowKey} ref={ref} style={{ zIndex }} className="absolute">
+                < Component {...props} />
+            </section >
+        );
+    };
+
+    Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`;
+
+    return Wrapped;
+};
+
+export default WindowWrapper;
